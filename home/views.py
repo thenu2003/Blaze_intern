09e5649dc34be9ddd96d1db9e9108309d141992f
Guru <mguruprasanth2004@gmail.com>
2023-07-11 17:54:50 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def loginn(request):
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+
+            return HttpResponse("logged successfully")
+        else:
+            return redirect("signup")
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method=="POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password=request.POST['password']
+        myuser=User.objects.create_user(username,email,password)
+        myuser.save()
+        return redirect('login')
+    return render(request,'signup.html')
