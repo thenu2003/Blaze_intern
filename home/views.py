5ad42d3328715bb4b9e2dcdf3946f79fb92195ec
Guru <mguruprasanth2004@gmail.com>
2023-07-23 01:05:34 +0530

    live data files added

--- home/views.py
+++ home/views.py
@@ -54,6 +54,7 @@ def live_data(request):
             ticker = 'XRP-USD'
         elif selected_option == 'DOGE':     # Updated to 'DOGE'
             ticker = 'DOGE-USD'
+        default_option = selected_option
         if ticker:
             data = yf.download(tickers=ticker, period='5d', interval='15m')
             latest_price = data.iloc[-1]['Close']
@@ -90,6 +91,7 @@ def live_data(request):
             current = round(current_price, 2)
 
             context = {
+                'selected_option': default_option,
                 'graph_html': graph_html,
                 'average': average_price,
                 'close_price': close,
@@ -101,14 +103,17 @@ def live_data(request):
    # For GET requests or when no option is selected 
     return render(request, 'live_data.html')
 
+
+
 def live_data1(request):
     if request.method == "POST":
         selected_option = request.POST.get('select', None)
         ticker = None
         if selected_option == 'NIFTY 50':    # Added Nifty ticker
             ticker = '^NSEI'  # Nifty 50 index
-        elif selected_option == 'NIFTY Bank':
+        elif selected_option == 'NIFTY Banks':
             ticker = '^NSEBANK'
+        default_option = selected_option
         if ticker:
             data = yf.download(tickers=ticker, period='5d', interval='15m')
             latest_price = data.iloc[-1]['Close']
@@ -145,6 +150,7 @@ def live_data1(request):
             current = round(current_price, 2)
 
             context = {
+                'selected_option': default_option,
                 'graph_html': graph_html,
                 'average': average_price,
                 'close_price': close,
@@ -156,6 +162,61 @@ def live_data1(request):
    # For GET requests or when no option is selected 
     return render(request, 'live_data1.html')
 
+# def live_data1(request):
+#     if request.method == "POST":
+#         selected_option = request.POST.get('select', None)
+#         ticker = None
+#         if selected_option == 'NIFTY 50':    # Added Nifty ticker
+#             ticker = '^NSEI'  # Nifty 50 index
+#         elif selected_option == 'NIFTY Bank':
+#             ticker = '^NSEBANK'
+#         if ticker:
+#             data = yf.download(tickers=ticker, period='5d', interval='15m')
+#             latest_price = data.iloc[-1]['Close']
+#             prices = data[['Open', 'High', 'Low', 'Close']]
+#             average = data['Close'].mean()
+#             average_price = round(average, 2)
+
+#             # Create a candlestick graph using Plotly
+#             fig = go.Figure(data=[go.Candlestick(x=data.index,
+#                                                 open=prices['Open'],
+#                                                 high=prices['High'],
+#                                                 low=prices['Low'],
+#                                                 close=prices['Close'])])
+
+#             # Customize the graph layout
+#             fig.update_layout(
+#                 title=f'{selected_option} Price',
+#                 xaxis_title='Date',
+#                 yaxis_title='Price',
+#                 showlegend=True
+#             )
+
+#             # Convert the graph to HTML and pass it to the template
+#             graph_html= fig.to_html(full_html=False, include_plotlyjs='cdn')
+
+#             # Fetch current close price (last available price)
+#             data = yf.download(tickers=ticker, period="1d", interval="1m")
+#             close_price = data['Close'][0]
+#             close = round(close_price, 2)
+
+#             # Fetch current real-time price
+#             datas = yf.download(tickers=ticker, period="1d", interval="1m")
+#             current_price = datas['Close'][-1]
+#             current = round(current_price, 2)
+
+#             context = {
+#                 'graph_html': graph_html,
+#                 'average': average_price,
+#                 'close_price': close,
+#                 'Current_price': current
+#             }
+
+#             return render(request, 'live_data1.html', context)
+
+#    # For GET requests or when no option is selected 
+#     return render(request, 'live_data1.html')
+
 def live_data2(request):
     if request.method == "POST":
         selected_option = request.POST.get('select', None)
@@ -164,6 +225,7 @@ def live_data2(request):
             ticker = 'GOOG'  # Nifty 50 index
         elif selected_option == 'Apple':
             ticker = 'AAPL'
+        default_option = selected_option
         if ticker:
             data = yf.download(tickers=ticker, period='5d', interval='15m')
             latest_price = data.iloc[-1]['Close']
@@ -200,6 +262,7 @@ def live_data2(request):
             current = round(current_price, 2)
 
             context = {
+                'selected_option': default_option,
                 'graph_html': graph_html,
                 'average': average_price,
                 'close_price': close,
@@ -210,6 +273,61 @@ def live_data2(request):
 
    # For GET requests or when no option is selected 
     return render(request, 'live_data2.html')
+
+# def live_data2(request):
+#     if request.method == "POST":
+#         selected_option = request.POST.get('select', None)
+#         ticker = None
+#         if selected_option == 'Google':    # Added Nifty ticker
+#             ticker = 'GOOG'  # Nifty 50 index
+#         elif selected_option == 'Apple':
+#             ticker = 'AAPL'
+#         if ticker:
+#             data = yf.download(tickers=ticker, period='5d', interval='15m')
+#             latest_price = data.iloc[-1]['Close']
+#             prices = data[['Open', 'High', 'Low', 'Close']]
+#             average = data['Close'].mean()
+#             average_price = round(average, 2)
+
+#             # Create a candlestick graph using Plotly
+#             fig = go.Figure(data=[go.Candlestick(x=data.index,
+#                                                 open=prices['Open'],
+#                                                 high=prices['High'],
+#                                                 low=prices['Low'],
+#                                                 close=prices['Close'])])
+
+#             # Customize the graph layout
+#             fig.update_layout(
+#                 title=f'{selected_option} Price',
+#                 xaxis_title='Date',
+#                 yaxis_title='Price',
+#                 showlegend=True
+#             )
+
+#             # Convert the graph to HTML and pass it to the template
+#             graph_html= fig.to_html(full_html=False, include_plotlyjs='cdn')
+
+#             # Fetch current close price (last available price)
+#             data = yf.download(tickers=ticker, period="1d", interval="1m")
+#             close_price = data['Close'][0]
+#             close = round(close_price, 2)
+
+#             # Fetch current real-time price
+#             datas = yf.download(tickers=ticker, period="1d", interval="1m")
+#             current_price = datas['Close'][-1]
+#             current = round(current_price, 2)
+
+#             context = {
+#                 'graph_html': graph_html,
+#                 'average': average_price,
+#                 'close_price': close,
+#                 'Current_price': current
+#             }
+
+#             return render(request, 'live_data2.html', context)
+
+#    # For GET requests or when no option is selected 
+#     return render(request, 'live_data2.html')
 
 def my_view(request):
     return render(request, 'my_template.html')
